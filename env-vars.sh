fc2698210ddd051f0e175c48fe0c21c8de20bd74
Gustavo Ciotto <gustavociotto@gmail.com>
2017-03-10 08:53:35 -0300

    minor updates

--- env-vars.sh
+++ env-vars.sh
@@ -2,8 +2,6 @@
 
 DOCKER_MANTAINER_NAME=gciotto
 DOCKER_NAME=alarm-server-activemq
-DOCKER_RUN_NAME=alarm-server-activemq-docker-container
-
+DOCKER_RUN_NAME=alarm-server-activemq
 ACTIVE_MQ_PORT=61616
-
 NETWORK_ID=alarm-server-internal-network
