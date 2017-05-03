7899206fb1a68264c86322bd76da6dca7b668ab9
Gustavo Ciotto <gustavociotto@gmail.com>
2017-05-03 15:50:07 -0300

    commentaries added

--- build-docker-alarm-activemq.sh
+++ build-docker-alarm-activemq.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-source env-vars.sh
+#
+# A simple script to build a container for the alarm system's jms server.
+#
+# Gustavo Ciotto Pinton
+# Controls Group - Brazilian Synchrotron Light Source Laboratory - LNLS
+#
+
+. ./env-vars.sh
 
 docker build -t ${DOCKER_MANTAINER_NAME}/${DOCKER_NAME} .
--- run-docker-alarm-activemq.sh
+++ run-docker-alarm-activemq.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-set -a
-set -e
-set -u
+#
+# An auxiliary script to start a new Docker container. It should be used to test the 
+# respective image. To deploy, use the composed repository, available in
+# https://github.com/lnls-sirius/docker-alarm-composed .  
+#
+# Gustavo Ciotto Pinton
+# Controls Group - Brazilian Synchrotron Light Source Laboratory - LNLS
+#
+. ./env-vars.sh
 
-source env-vars.sh
-
+# Verifies if a container with the same name is already running.
 CONTAINERS=$(docker ps -a | grep ${DOCKER_RUN_NAME})
 
 if [ ! -z ${CONTAINERS:+x} ]; then
+
+    echo "A container with the name ${DOCKER_RUN_NAME} is already running..."
+
+    # Stops running container and deletes it.
+    echo "Executing 'docker stop ${DOCKER_RUN_NAME}' ..."
     docker stop ${DOCKER_RUN_NAME}
+
+    echo "Executing 'docker rm ${DOCKER_RUN_NAME}' ..."
     docker rm ${DOCKER_RUN_NAME}
 fi
 
